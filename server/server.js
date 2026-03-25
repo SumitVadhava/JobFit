6be60af0808deef66828062ef516d0ddfd4f2d63
Sumit  Vadhava <140806339+SumitVadhava@users.noreply.github.com>
2026-03-25 19:33:09 +0530

    Refactor server.js for improved structure and clarity

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.use(
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173" || "http://localhost:3000" ,
     credentials: true,
   }),
 );
