5422b8660d484c0fe1115a1b08a8fc187f968de5
Manavkotecha <manavkotecha45@gmail.com>
2026-03-29 14:44:26 +0530

    candidate profile model fixed

--- server/server.js
+++ server/server.js
@@ -25,6 +25,7 @@ const allowedOrigins = [
   "https://jobfit-delta.vercel.app",
   "http://localhost:5173",
   "http://localhost:3000",
+  "http://localhost:5000",
   "https://jobfit-s5v7.onrender.com"
 ];
 
